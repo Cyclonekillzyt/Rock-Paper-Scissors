07a583352121b74753deaa7d2e424d27ad9ba46d
Cyclonekillzyt <kendrickscyclone@gmail.com>
2025-01-06 22:22:30 +0000

    first function actually done

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const rock = "rock";
-const paper = "paper";
-const scissors = "scissors";
+const rock = "R";
+const paper = "P";
+const scissors = "S";
 function getComputerChoice(){
     const possibleMoves = [rock , paper, scissors]
     let computerChoice = Math.floor(Math.random() * possibleMoves.length);
     return possibleMoves[computerChoice];
+}
+function getHumanChoice(){
+    let humanChoice = prompt("Enter your choice: R, P or S").toUpperCase();
+    return humanChoice;
 }
