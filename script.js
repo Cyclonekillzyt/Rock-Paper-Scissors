3b3e8afa66a2ccfb0c60262b04d6430e229a6f09
Cyclonekillzyt <kendrickscyclone@gmail.com>
2025-01-06 22:18:50 +0000

    first function done

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hello world!");
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
+function getComputerChoice(){
+    const possibleMoves = [rock , paper, scissors]
+    let computerChoice = Math.floor(Math.random() * possibleMoves.length);
+}
