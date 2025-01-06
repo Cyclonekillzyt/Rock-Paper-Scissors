580309b276d1356822cd3c03ba7d8f964f66812b
Cyclonekillzyt <kendrickscyclone@gmail.com>
2025-01-06 22:42:07 +0000

    writting the function to play a a round of the game

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ let humanScore = 0;
 let computerScore = 0;
 const possibleMoves = [rock, paper, scissors];
 
-function getComputerChoice() {
+const computerSelection = function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * possibleMoves.length);
   return possibleMoves[computerChoice];
 }
-function getHumanChoice() {
+const humanSelection = function getHumanChoice() {
   let humanChoice = prompt("Enter your choice: R, P or S").toUpperCase();
   if (humanChoice == "R" || humanChoice == "P" || humanChoice == "S") {
     return humanChoice;
@@ -23,9 +23,14 @@ function playRound(humanChoice, computerChoice) {
   if (humanChoice == computerChoice) {
     alert("It's a draw");
     console.log("It's a draw");
-  }else if (humanChoice == rock && computerChoice == scissors) {
+  }else if ((humanChoice == rock && computerChoice == scissors) || (humanChoice == paper && computerChoice == rock) || (humanChoice == scissors && computerChoice == paper)) {
     humanScore++;
-    alert("You win! Rock beats Scissors");
-    console.log("You win! Rock beats Scissors");
+    alert(`You win! ${humanChoice} beats ${computerChoice}`);
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
   }
+    else {
+        computerScore++;
+        alert(`You lose! ${computerChoice} beats ${humanChoice}`);
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
 }
