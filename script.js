57ad72f34b2212af6f48f8313e970419b1c0f595
Cyclonekillzyt <kendrickscyclone@gmail.com>
2025-01-12 20:23:59 +0000

    lets do the js

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 const startBtn = document.getElementById("start");
 const resetBtn = document.getElementById("reset");
+const computer = document.getElementById("computerScore");
+const player = document.getElementById("playerScore");
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
 const rock = "ROCK";
 const paper = "PAPER";
 const scissors = "SCISSORS";
 const possibleMoves = [rock, paper, scissors];
+let humanChoice = "";
+rockBtn.onclick = () =>{
+  humanChoice = rock;
+  playGame();
+}
+paperBtn.onclick = () =>{
+  humanChoice = paper;
+  playGame();
+}
+scissorsBtn.onclick = () =>{
+  humanChoice = scissors;
+  playGame();
+}
+
 
 const computerSelection = function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * possibleMoves.length);
   return possibleMoves[computerChoice];
 };
 const humanSelection = function getHumanChoice() {
-  let humanChoice = prompt(
-    "Enter your choice: ROCK, PAPER or SCISSORS"
-  ).toUpperCase();
   if (
     humanChoice == "ROCK" ||
     humanChoice == "PAPER" ||
@@ -25,9 +41,13 @@ const humanSelection = function getHumanChoice() {
   }
 };
 
+
+
 function playGame() {
   let humanScore = 0;
   let computerScore = 0;
+  computer.innerHTML = computerScore;
+  player.innerHTML = humanScore;
   function playRound(humanChoice, computerChoice) {
     if (humanChoice == computerChoice) {
       console.log("It's a draw");
