bf4f258b3def4a474b53bec62bc52351b44a41e0
Cyclonekillzyt <kendrickscyclone@gmail.com>
2025-01-26 03:02:26 +0000

    Almost Done

--- script.js
+++ script.js
@@ -10,36 +10,53 @@ const resultText = document.getElementById("resultText");
 const resultComparison = document.getElementById("resultComparison");
 const container = document.getElementById("container");
 const round = document.getElementById("round");
-const totalScore = document.getElementById("total")
+const totalScore = document.getElementById("total");
+const firstImage = document.getElementById("first");
+const secondImage = document.getElementById("second");
 const rock = "ROCK";
 const paper = "PAPER";
 const scissors = "SCISSORS";
 const possibleMoves = [rock, paper, scissors];
-
+secondImage.classList.add("reverse");
 
 let currentRound = 0;
 let humanScore = 0;
 let computerScore = 0;
 let total = 0;
 let humanChoice = "";
+let computerChoice = "";
 
-rockBtn.onclick = () => {
+rockBtn.addEventListener("click", () => {
   humanChoice = rock;
-  playGame();
-};
+  resetImage();
+  startingAnimation();
+  setTimeout(playGame, 3000);
+  setTimeout(changeImage, 3000);
+});
+
 paperBtn.onclick = () => {
   humanChoice = paper;
-  playGame();
+  resetImage();
+  startingAnimation();
+  setTimeout(playGame, 3000);
+  setTimeout(changeImage, 3000);
 };
+
 scissorsBtn.onclick = () => {
   humanChoice = scissors;
-  playGame();
+  resetImage();
+  startingAnimation();
+  setTimeout(playGame, 3000);
+  setTimeout(changeImage, 3000);
 };
 
 const computerSelection = function getComputerChoice() {
-  let computerChoice = Math.floor(Math.random() * possibleMoves.length);
-  return possibleMoves[computerChoice];
+  let randomChoice = Math.floor(Math.random() * possibleMoves.length);
+  let choice = possibleMoves[randomChoice];
+  computerChoice = choice;
+  return choice;
 };
+
 const humanSelection = function getHumanChoice() {
   if (
     humanChoice == "ROCK" ||
@@ -47,20 +64,18 @@ const humanSelection = function getHumanChoice() {
     humanChoice == "SCISSORS"
   ) {
     return humanChoice;
-  } else {
-    alert("Invalid choice. Please enter  ROCK, PAPER or SCISSORS");
-    return getHumanChoice();
   }
 };
 
 function playGame() {
   function playRound(humanChoice, computerChoice) {
     if (humanChoice == computerChoice) {
-      resultText.textContent = " It's a draw";
+      resultText.textContent = "It's a draw";
       currentRound++;
       round.textContent = currentRound;
       total++;
       totalScore.textContent = total;
+      rps.textContent = humanChoice;
     } else if (
       (humanChoice == rock && computerChoice == scissors) ||
       (humanChoice == paper && computerChoice == rock) ||
@@ -71,30 +86,33 @@ function playGame() {
       player.textContent = humanScore;
       currentRound++;
       round.textContent = currentRound;
-      total = humanScore + computerScore;
       totalScore.textContent = total;
+      rps.textContent = humanChoice;
     } else {
       computerScore++;
       resultText.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
       computer.textContent = computerScore;
       currentRound++;
       round.textContent = currentRound;
-      total = humanScore + computerScore;
       totalScore.textContent = total;
+      rps.textContent = computerChoice;
     }
   }
-  let humanChoice = humanSelection();
-  let computerChoice = computerSelection();
+  humanChoice = humanSelection();
+  computerChoice = computerSelection();
   playRound(humanChoice, computerChoice);
   computer.innerText = computerScore;
   player.innerText = humanScore;
 }
 
-resetBtn.onclick = (function finalScore() {
+resetBtn.onclick = function finalScore() {
   currentRound = 0;
   humanScore = 0;
   computerScore = 0;
-})();
+  computer.textContent = computerScore;
+  player.textContent = humanScore;
+  totalScore.textContent = 0;
+};
 
 startBtn.addEventListener("click", () => {
   startBtn.style.display = "none";
@@ -103,4 +121,47 @@ startBtn.addEventListener("click", () => {
   currentRound = 1;
 });
 
-//playGame();
+startBtn.style.display = "none";
+resetBtn.style.display = "block";
+container.style.display = "block";
+
+function startingAnimation() {
+  firstImage.classList.add("left");
+  secondImage.classList.add("right");
+  setTimeout(() => {
+    firstImage.classList.remove("left");
+    secondImage.classList.remove("right");
+  }, 3000);
+}
+
+function changeImage() {
+  switch (humanChoice) {
+    case "ROCK":
+      firstImage.src = "pictures/rock.png";
+      break;
+    case "PAPER":
+      firstImage.src = "pictures/paper.png";
+      break;
+    case "SCISSORS":
+      firstImage.src = "pictures/scissors.png";
+      break;
+  }
+  switch (computerChoice) {
+    case "ROCK":
+      secondImage.src = "pictures/rock.png";
+      break;
+    case "PAPER":
+      secondImage.src = "pictures/paper.png";
+      break;
+    case "SCISSORS":
+      secondImage.src = "pictures/scissors.png";
+      break;
+  }
+}
+
+function resetImage() {
+  firstImage.src = "pictures/rock.png";
+  secondImage.src = "pictures/rock.png";
+}
+
+console.log(computerChoice);
